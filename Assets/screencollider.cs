200151fd8587fe4537abdb2907a77d987ab49880
Taust <116812445+Tausty@users.noreply.github.com>
2024-07-29 19:28:33 -0400

    Bird Movment and incomplete rotation
    Rotation does not work as intended. fix urgently, otherwise continue work on main concept (whatever that is lol)

---
+++ Assets/screencollider.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class screencollider : MonoBehaviour
+{
+/*    EdgeCollider2D myec;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myec = GetComponent<EdgeCollider2D>();
+        createEdges(myec);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void createEdges(EdgeCollider2D myec) {
+        List<Vector2> edges = new List<Vector2>();
+        edges.Add(Camera.main.ScreenToWorldPoint(Vector2.zero));
+        edges.Add(Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0)));
+        edges.Add(Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height)));
+        edges.Add(Camera.main.ScreenToWorldPoint(new Vector2(0, Screen.height)));
+        edges.Add(Camera.main.ScreenToWorldPoint(Vector2.zero));
+        myec.SetPoints(edges);
+    }*/
+}
